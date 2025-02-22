835d38793ac42c0b9d830dedab271c93b2280170
thegbars <gbarsotti262@gmail.com>
2025-02-21 23:39:40 -0500

    sandbox page

--- teambuilder/src/app/page.tsx
+++ teambuilder/src/app/page.tsx
@@ -1,5 +1,6 @@
 import "./css/mainPage.scss";
 import { Button } from "@/components/ui/button"
+import Link from 'next/link';
 
 
 export default function Home() {
@@ -7,7 +8,9 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center  h-screen">
       <h1 className="text-8xl">Welcome to Team Builder</h1>
       <div className="flex my-8 gap-20">
-        <Button size="wide">About</Button>
+        <Link href="/sandbox">
+          <Button size="wide">sandbox</Button>
+        </Link>
         <a href="">
           <Button size="wide">
             Get Started &rarr;
--- teambuilder/src/app/pages/attack.tsx
+++
@@ -1,7 +0,0 @@
-export default function Attack() {
-    return (
-        <div>
-            <h1>Attack</h1>
-        </div>
-    );
-}
