6621c060e132c017b08c8cd1d1480a2116c678a6
thegbars <gbarsotti262@gmail.com>
2025-02-21 19:26:37 -0500

    Button size increased

--- teambuilder/src/components/ui/button.tsx
+++ teambuilder/src/components/ui/button.tsx
@@ -24,6 +24,8 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xxl: "h-20 rounded-md px-8",
+        wide: "h-20 rounded-md px-20",
         icon: "h-9 w-9",
       },
     },
